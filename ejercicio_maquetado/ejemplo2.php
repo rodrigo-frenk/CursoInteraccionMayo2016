687788b55f8faedf4c662a9f3191c6223fe860a0
furenku <furenku@gmail.com>
2016-05-21 13:16:03 -0500

    estructura basica pra mi primer modulo

---
+++ ejercicio_maquetado/ejemplo2.php
@@ -0,0 +1,35 @@
+<?php include_once 'header.php'; ?>
+
+<section id="ejemplo" class="row h_100">
+
+
+<!-- .caja1.small-12.columns.h_60 -->
+<div class="caja1 small-12 columns h_60">
+
+   <!-- .subcaja.small-4.columns*3 -->
+   <div class="subcaja small-4 columns">
+      <!-- .subsubcaja.h_25*4 -->
+      <div class="subsubcaja h_25">
+         <!-- .subsubsubcaja.small-4.columns -->
+         <div class="subsubsubcaja small-4 columns"></div>
+         <!-- .subsubsubcaja.small-8.columns -->
+         <div class="subsubsubcaja small-8 columns"></div>
+      </div>
+      <div class="subsubcaja h_25"></div>
+      <div class="subsubcaja h_25"></div>
+      <div class="subsubcaja h_25"></div>
+   </div>
+   <div class="subcaja small-4 columns"></div>
+   <div class="subcaja small-4 columns"></div>
+
+</div>
+
+<!-- .caja2.small-12.columns.h_40 -->
+<div class="caja2 small-12 columns h_40">
+
+</div>
+
+
+</section>
+
+<?php include_once 'footer.php'; ?>
---
+++ ejercicio_maquetado/mi_primer_modulo.php
@@ -0,0 +1,15 @@
+<?php include_once 'header.php'; ?>
+
+<div class="titulo h_20">
+   <h1>Mi primer módulo</h1>
+</div>
+
+<!-- .vista_usuario.small-12.medium-6.large-4.columns.h_80 -->
+<article class="vista_usuario small-12 medium-6 large-4 columns h_80">
+
+<!-- A continuación el código: -->
+
+
+</article>
+
+<?php include_once 'footer.php'; ?>
